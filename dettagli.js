ef69044d780518b17154bb8050d1be464e318898
GabrielAzamfiri <167236687+GabrielAzamfiri@users.noreply.github.com>
2024-06-13 18:58:25 +0200

    add btn dettagli e ritorno al home

--- dettagli.js
+++ dettagli.js
@@ -9,12 +9,30 @@ window.addEventListener("DOMContentLoaded", () => {
       console.log(picObj);
 
       const container = document.querySelector("#img");
-      container.style.backgroundColor = picObj.avg_color;
-      console.log(container);
+      const section = document.querySelector("section");
+
+      section.style.backgroundColor = picObj.avg_color;
+
       const img = document.createElement("img");
       img.className = "w-100 mb-5";
       img.src = picObj.src.original;
       img.alt = picObj.alt;
-      container.appendChild(img);
+      const description = document.createElement("h3");
+      description.className = "text-center mb-5";
+      description.innerText = picObj.alt;
+
+      const divBtn = document.createElement("div");
+      divBtn.className = "d-flex justify-content-between";
+
+      const photographerLink = document.createElement("a");
+      photographerLink.href = picObj.photographer_url;
+      photographerLink.innerText = "More info about " + picObj.photographer;
+      photographerLink.className = "btn btn-primary mb-3 fs-5 border border-dark";
+      const home = document.createElement("a");
+      home.href = "./pexels-start.html";
+      home.innerText = "Home";
+      home.className = "btn btn-primary mb-3 fs-5 border border-dark ";
+      divBtn.append(photographerLink, home);
+      container.append(img, description, divBtn);
     });
 });
